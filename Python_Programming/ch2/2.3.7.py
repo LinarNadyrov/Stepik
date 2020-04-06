b5ad6c7eafb482b63e9e09af31c1daf3150dbfc8
LinarNadyrov <linar.nadyrov@itbegin.ru>
2020-04-06 16:55:39 +0300

    fixed homework-2

--- Python_Programming/ch2/2.3.7.py
+++ Python_Programming/ch2/2.3.7.py
@@ -9,3 +9,17 @@ for i in range(a,b+1):
         sum += i
         j += 1
 print(sum / j)
+
+print("============================================")
+
+a,b=(int(i) for i in input().split())
+sum = 0
+j = 0
+for i in range (a,b+1):
+    if i % 3 == 0:
+        sum+=i
+        j+=1
+        print(i)
+print("Сумма чисел  = ", sum)
+print("Количество чисел = ", j)
+print(sum/j)
