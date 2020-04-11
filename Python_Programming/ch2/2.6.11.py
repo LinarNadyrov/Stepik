8a9ce5831755cf135f57dbd0e1e3cfe225c2216b
LinarNadyrov <linar.nadyrov@itbegin.ru>
2020-04-11 10:42:01 +0300

    fixed 3 homework-2

--- Python_Programming/ch2/2.6.11.py
+++ Python_Programming/ch2/2.6.11.py
@@ -1,12 +1,38 @@
-Дополнительная
-
-Выведите таблицу размером n×nn×n, заполненную числами от 11 до n2n2 по спирали, выходящей из левого верхнего угла и закрученной по часовой стрелке, как показано в примере (здесь n=5n=5):
-Sample Input:
-5
-Sample Output:
-1 2 3 4 5
-16 17 18 19 6
-15 24 25 20 7
-14 23 22 21 8
-13 12 11 10 9
-Нажмите, чтобы начать решать ✍
+n = int(input())
+matrix = [[0] * n for i in range(n)]
+num = 1
+end = n
+start = 0
+while num != num**2:
+    for i in range(start, end):
+        if num == num**2:
+            break
+        for j in range(start, end):
+            if num == num**2:
+                break
+            matrix[i][j] = num
+            num += 1
+            if j + 1 == end:
+                for k in range(start+1, end):
+                    if num == num**2:
+                        break
+                    matrix[k][j] = num
+                    num += 1
+                    if k + 1 == end:
+                        for l in range(-end+1, -start):
+                            if num == num**2:
+                                break
+                            matrix[k][l] = num
+                            num += 1
+                            if l + 1 == start:
+                                for m in range(-end+1, -start-1):
+                                    if num == num**2:
+                                        break
+                                    matrix[m][l] = num
+                                    num += 1
+        end -= 1
+        start += 1
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j], end='\t')
+    print()
